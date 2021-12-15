a6c5eaed58d4da5cc616a1039ac390cc7cbd64cd
gnsia <gnsia@naver.com>
2021-12-15 13:30:54 +0900

    Spring security 찍먹 login, register 구현 authenticated에 따른 처리

---
+++ src/main/java/com/example/myhome/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.myhome.repository;
+
+import com.example.myhome.model.Board;
+import com.example.myhome.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
